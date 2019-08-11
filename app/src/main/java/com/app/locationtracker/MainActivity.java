94e8b4e46d8b5be3e8120b6dcd76c61ab2020495
AnandBoreda <zz.anand2016@gmail.com>
2019-08-11 16:47:19 +0530

    updated: button is working and second screen is activating updated: share button working

--- app/src/main/java/com/app/locationtracker/MainActivity.java
+++ app/src/main/java/com/app/locationtracker/MainActivity.java
@@ -6,14 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "body";
+                String shareSub = "sub";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(myIntent, "Share Using"));
+            }
+        });
 
         button = findViewById(R.id.start_button);
         button.setOnClickListener(new View.OnClickListener() {
