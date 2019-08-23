be0e0c4ffc6cec078c23567c0340cae41bb57412
AnandBoreda <zz.anand2016@gmail.com>
2019-08-23 13:03:01 +0530

    RetrieveFeedTask renamed to Locator
    siren activity added

--- app/src/main/java/com/app/locationtracker/Locator.java
+++ app/src/main/java/com/app/locationtracker/Locator.java
@@ -1,80 +1,78 @@
-
 package com.app.locationtracker;
 
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
-
-import android.content.Intent;
-import android.os.Bundle;
+import android.os.AsyncTask;
 import android.util.Log;
-import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-
-public class Locator extends AppCompatActivity{
-    private InterstitialAd mInterstitialAd;
-    String API_URL = "http://apilayer.net/api/validate";
-    String API_KEY = "bdcf9ba9875952058f4daed04367006a";
+class Locator extends AsyncTask<Void, Void, String> {
+    private String API_URL = "http://apilayer.net/api/validate";
+    private String API_KEY = "bdcf9ba9875952058f4daed04367006a";
+    EditText etext;
+    Button butt;
+    TextView number;
     Spinner countryCode;
-    EditText number;
-    AdView mAdView;
+
+    private Exception exception;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_locator);
-
-        number = findViewById(R.id.mobile_number);
-        countryCode = findViewById(R.id.country_code);
-
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544/1033173712");
-        //ca-app-pub-3940256099942544/8691691433
-        mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        mInterstitialAd.loadAd(new AdRequest.Builder().build());
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override public void onAdClosed()
-            { mInterstitialAd.loadAd(new AdRequest.Builder().build()); } });
-
-
+    protected String doInBackground(Void... voids) {
+        try {
+           URL url = new URL(API_URL + "?access_key=" + API_KEY + "&number=" + number + "&country_code=" + countryCode);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
         }
-
-    public void Intertialshow()
-    { if (mInterstitialAd.isLoaded()) { mInterstitialAd.show(); }
-    else
-    { finish(); } }
-
-    @Override
-    public void onBackPressed()
-    {
-        Intertialshow();
-        startActivity(new Intent (this, Home.class));
     }
-    public void find(View v){
 
-        RetrieveFeedTask retrieveFeedTask = new RetrieveFeedTask();
-        retrieveFeedTask.doInBackground();
-        //retrieveFeedTask.onPostExecute();
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+            //Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.i("INFO", response);
+        try {
+            JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+            String countryname = object.getString("country_name");
+            int location = object.getInt("location");
+            String carrier = object.getString("carrier");
+            String line_type = object.getString("line_type");
+
+            //Set the text to view from here
+            // TODO Add to specific text views in the XML file
+            // Like countrycode -> country code's text view etc
+        } catch (JSONException e) {
+            Log.w("INFO", response);
+        }
+        // responseView.setText(response);
     }
+
 }
-
-
-
--- app/src/main/java/com/app/locationtracker/PoliceSirenMain.java
+++ app/src/main/java/com/app/locationtracker/PoliceSirenMain.java
@@ -2,13 +2,39 @@ package com.app.locationtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PoliceSirenMain extends AppCompatActivity {
+    Handler color = new Handler();
+    ImageView red,blue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_police_siren_main);
+        red = findViewById(R.id.colorred);
+        blue = findViewById(R.id.colorblue);
+
+
+        for (int k = 1; k < 100; k++) {
+            for (int i = 1; i < 2; i++) {
+                for (int j = 1; j < 2; j++) {
+                    color.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            blue.setVisibility(View.VISIBLE);
+                            red.setVisibility(View.INVISIBLE);
+
+                        }
+                    }, 500);
+                }
+                red.setVisibility(View.VISIBLE);
+                blue.setVisibility(View.INVISIBLE);
+            }
+        }
     }
 }
