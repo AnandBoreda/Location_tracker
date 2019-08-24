438705d7cb3991a59db990597f68bc94ddb29e22
AnandBoreda <zz.anand2016@gmail.com>
2019-08-25 01:09:17 +0530

    crash -- MainActivity.java

--- app/src/main/java/com/app/locationtracker/MainActivity.java
+++ app/src/main/java/com/app/locationtracker/MainActivity.java
@@ -7,11 +7,15 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.NativeExpressAdView;
+import com.google.android.gms.ads.VideoController;
+import com.google.android.gms.ads.VideoOptions;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     AdView mAdView;
     Button button;
     Button bt;
+    private static String LOG_TAG = "EXAMPLE";
+
+    NativeExpressAdView mAdViewNative;
+    VideoController mVideoController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +48,43 @@ public class MainActivity extends AppCompatActivity {
             @Override public void onAdClosed()
             { mInterstitialAd.loadAd(new AdRequest.Builder().build()); } });
 
-        //woohoo
+
+
+        //native
+        // Locate the NativeExpressAdView.
+        mAdViewNative = (NativeExpressAdView) findViewById(R.id.adView);
+
+// Set its video options.
+        mAdViewNative.setVideoOptions(new VideoOptions.Builder()
+                .setStartMuted(true)
+                .build());
+
+// The VideoController can be used to get lifecycle events and info about an ad's video
+// asset. One will always be returned by getVideoController, even if the ad has no video
+// asset.
+        mVideoController = mAdViewNative.getVideoController();
+        mVideoController.setVideoLifecycleCallbacks(new VideoController.VideoLifecycleCallbacks() {
+            @Override
+            public void onVideoEnd() {
+                Log.d(LOG_TAG, "Video playback is finished.");
+                super.onVideoEnd();
+            }
+        });
+
+// Set an AdListener for the AdView, so the Activity can take action when an ad has finished
+// loading.
+        mAdViewNative.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                if (mVideoController.hasVideoContent()) {
+                    Log.d(LOG_TAG, "Received an ad that contains a video asset.");
+                } else {
+                    Log.d(LOG_TAG, "Received an ad that does not contain a video asset.");
+                }
+            }
+        });
+
+        mAdView.loadAd(new AdRequest.Builder().build());
 
         MobileAds.initialize(this, "ca-app-pub-8212638041206076~2499605568");
 
